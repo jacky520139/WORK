418f28af820a4d646a9215984f92e104e04a3ca8
jacky520139 <542276603@qq.com>
2019-11-21 09:58:06 +0800

    1

---
+++ 程序/ble_3435_sdk_ext_39_0D0D/projects/ble_app_gatt/obj/rtc.d
@@ -0,0 +1,6 @@
+.\obj\rtc.o: ..\..\sdk\plactform\driver\rtc\rtc.c
+.\obj\rtc.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\obj\rtc.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+.\obj\rtc.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+.\obj\rtc.o: ..\..\sdk\plactform\arch\BK3435_reg.h
+.\obj\rtc.o: ..\..\sdk\plactform\driver\rtc\rtc.h
